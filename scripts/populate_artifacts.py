0a981febf4869a7f7063e97ebac45bbcceed8a34
diana <dchumpitazm@gmail.com>
2018-11-11 17:32:35 -0500

    scripts: Add script to populate or update artifacts

---
+++ scripts/populate_artifacts.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+import argparse
+import csv
+from pymongo import MongoClient
+from pymongo.errors import DuplicateKeyError
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--artifacts-file",
+                        type=argparse.FileType('r', encoding='utf-8'),
+                        help="Path to the config file with features to use",
+                        required=True)
+    parser.add_argument("-u", "--update",
+                        action="store_true",
+                        default=False,
+                        required=False,
+                        help="Update if the name exists.")
+    args = parser.parse_args()
+
+
+    artifacts_info = csv.reader(args.artifacts_file)
+
+    client = MongoClient()
+    client = MongoClient("localhost", 27017)
+    db = client.testdb
+
+    for i, row in enumerate(artifacts_info):
+        if i == 0:
+            continue
+        type_ = row[0]
+        name = row[1]
+        latitude = float(row[2].replace(".", ""))
+        longitude = float(row[3].replace(".", ""))
+
+        data = {
+            "type": type_,
+            "name": name,
+            "latitude": latitude,
+            "longitude": longitude
+        }
+        try:
+            id_ = db.artifacts.insert_one(data.copy())
+            print(id_)
+        except DuplicateKeyError:
+            print("Cannot insert data for {}, because it exists.".format(name))
+            if args.update:
+                print("Update data for artifact with name {}".format(name))
+                del data["name"]
+
+                print(data)
+                db.artifacts.update_one({"name": name}, {"$set": data})
+    
