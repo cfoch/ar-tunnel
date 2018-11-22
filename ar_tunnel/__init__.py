d6c440c3842e7411e4e9abeeba0a7deddf356b1e
Fabian Orccon <cfoch.fabian@gmail.com>
2018-11-22 13:04:37 +0000

    indexes: Fix misspelled index for users_artifacts

--- ar_tunnel/__init__.py
+++ ar_tunnel/__init__.py
@@ -5,7 +5,7 @@ from flask_restplus import Api
 def init_db(mongo):
     mongo.db.users.create_index("PersonalInfo.email", unique=True)
     mongo.db.users.create_index("PersonalInfo.nickname", unique=True)
-    mongo.db.users_artifacts.create_index([("userd_id", 1), ("artifact_id", 1)],
+    mongo.db.users_artifacts.create_index([("user_id", 1), ("artifact_id", 1)],
                                           unique=True)
     mongo.db.artifacts.create_index("name", unique=True)
 
